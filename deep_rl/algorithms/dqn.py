9489532fca76f17ac37f0c9267b9e96ebe054c10
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2021-06-17 18:28:05 +0200

    DQN working

--- deep_rl/algorithms/dqn.py
+++ deep_rl/algorithms/dqn.py
@@ -1,18 +1,9 @@
 import torch
-from torch import optim
-from torch.distributions import Categorical
 
 from deep_rl.common import MLP, rewards_to_returns
-from deep_rl.common.mpi import gather
 import numpy as np
 import random
 from collections import deque
-
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-RANK = comm.Get_rank()
-SIZE = comm.Get_size()
 
 
 class Dataset:
@@ -20,22 +11,21 @@ class Dataset:
         self.transitions = deque(maxlen=100000)
 
     def store(self, transition):
-        transitions = gather([transition])
-        # = [[s1, a1, r1, s1'], [s2, a2, r2, s2'], ...]
-        if RANK == 0:  # only store in root
-            self.transitions += transitions
+        self.transitions.append(transition)
 
     def sample(self, batch_size):
+        batch_size = min(batch_size, len(self.transitions))
         transitions = random.sample(self.transitions, batch_size)
-        states = [transition[0] for transition in transitions]
-        actions = [transition[1] for transition in transitions]
-        rewards = [transition[2] for transition in transitions]
-        next_states = [transition[3] for transition in transitions]
-        return states, actions, rewards, next_states
+        states = torch.FloatTensor([t[0] for t in transitions])
+        actions = torch.LongTensor([t[1] for t in transitions]).unsqueeze(1)
+        rewards = torch.FloatTensor([t[2] for t in transitions])
+        next_states = torch.FloatTensor([t[3] for t in transitions])
+        dones = torch.BoolTensor([t[4] for t in transitions])
+        return states, actions, rewards, next_states, dones
 
 
 class DQN:
-    def __init__(self, env, lr=0.01, epsilon_decay=0.99, tau=10):
+    def __init__(self, env):
         """Implementation of DQN algorithm.
 
         Args:
@@ -44,104 +34,106 @@ class DQN:
             epsilon_decay (float, otpional): epsilon_decay.
         """
         self.env = env
-        self.main_Q = MLP(
-            [self.env.observation_space.shape[0], 4, self.env.action_space.n]
-        )
-        self.target_Q = MLP(
-            [self.env.observation_space.shape[0], 4, self.env.action_space.n]
-        )
-        self._is_mpi_root = RANK == 0
-        self.epsilon_decay = epsilon_decay
-        self.tau = tau
-        self.gamma = 0.99
-        self.dataset = Dataset()
 
-        # Since the leanring is only on root, no need
-        # to define optimizer and baseline_mean on other workers
-        if self._is_mpi_root:
-            self.criterion = torch.nn.L1Loss()
-            self.optimizer = optim.SGD(self.main_Q.parameters(), lr=lr)
+        self.dataset = Dataset()
+        input_size = self.env.observation_space.shape[0]
+        output_size = self.env.action_space.n
+        self.Q_main = MLP([input_size, 64, 64, output_size])  # main
+        self.Q_targ = MLP([input_size, 64, 64, output_size])  # target
+        self.batch_size = 64
+        self.criterion = torch.nn.L1Loss()
+        self.optimizer = torch.optim.SGD(self.Q_main.parameters(), lr=0.01)
+
+        self.epsilon_start = 0.9
+        self.epsilon_decay = 0.9999
+        self.epsilon_min = 0.05
+        self.tau = 100
+        self.gamma = 0.99
 
     def act(self, state, epsilon=0):
-        """Returns the action sample by the agent."""
+        """Espilon-greedy policy w.r.t. Q_main.
+
+        Args:
+            state (list): state as list.
+            epsilon (float): probability of taking random action.
+        """
         state = torch.FloatTensor(state)
         if random.random() > epsilon:  # epsilon greedy policy
-            values = self.main_Q(state)
-            action = torch.argmax(values).item()
+            with torch.no_grad():  # the grad should not propagate into action choice
+                values = self.Q_main(state)
+                action = torch.argmax(values).item()
         else:
             action = self.env.action_space.sample()
         return action
 
-    def sample_episodes(self, num_timesteps, epsilon):
-        """Sample episode and store them inn the dataset."""
-        timesteps_left = num_timesteps
-        state = self.env.reset()
-        done = False
-        while timesteps_left > 0:
-            # interract
-            action = self.act(state, epsilon)
-            next_state, reward, done, info = self.env.step(action)
-            # store
-            self.dataset.store([state, action, reward, next_state])
+    def train(self, nb_timesteps, test_fq=1000, nb_test_episodes=100, verbose=False):
+        """Train the agent.
 
-            state = next_state
-            timesteps_left -= 1
-
+        Args:
+            nb_timesteps (int): Number of training timesteps.
+            test_fq (int): test every test_fq timesteps.
+            nb_test_episodes (int): number of testing episodes.
+        """
+        # Start with epsilon = 1
+        epsilon = self.epsilon_start
+        test_returns = []
+        done = True
+        for timestep in range(nb_timesteps):
             if done:
                 # reset and loop again
                 state = self.env.reset()
                 done = False
+            # Sample episodes
+            action = self.act(state, epsilon)
+            next_state, reward, done, info = self.env.step(action)
+            # store the transition
+            self.dataset.store([state, action, reward, next_state, done])
+            # next state becomes current state
+            state = next_state
 
-    def train(self, nb_timesteps, test_fq, nb_test_rollout=100):
-        nb_epochs = nb_timesteps // test_fq
-        nb_timesteps_per_epoch = nb_timesteps // nb_epochs
-        # share the training in all workers
-        nb_timesteps_per_rollout = nb_timesteps_per_epoch // SIZE
-        epsilon = 1
-        success_rates = []
-        for epoch in range(nb_epochs):
-            self.sample_episodes(nb_timesteps_per_rollout, epsilon)
-            for _ in range(1000):
-                if self._is_mpi_root:  # perform learning in root worker
-                    states, actions, rewards, next_states = self.dataset.sample(256)
-                    states = torch.FloatTensor(states)
-                    actions = torch.tensor(actions)
-                    rewards = torch.IntTensor(rewards)
-                    next_states = torch.FloatTensor(next_states)
-                    # target = r + γ max_a′(Q_targ(s′,a′))
-                    target = rewards + self.gamma * torch.max(
-                        self.target_Q(next_states)
+            # get minibatch from the dataset
+            states, actions, rewards, next_states, dones = self.dataset.sample(self.batch_size)
+            # target = r + γ max_a′(Q_targ(s′,a′)) if s' is non terminal
+            # target = r + 0                       otherwise
+            next_state_values = torch.max(self.Q_targ(next_states), dim=1)[0]
+            next_state_values[dones] = 0
+            target = rewards + self.gamma * next_state_values
+            # prediction = Q(s′,a′)
+            prediction = self.Q_main(states).gather(1, actions).squeeze()
+            # loss = |target - prediction|
+            loss = self.criterion(target, prediction)
+            # Optimize the model : theta <- theta - grad_theta*loss
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # if loss < 1.0:
+            if timestep % self.tau == 0:
+                # Q_targ <- Q_main
+                self.Q_targ.load_state_dict(self.Q_main.state_dict())
+
+            # decay epsilon
+            epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
+
+            # test and print mean return
+            if timestep % test_fq == 0:
+                test_return = self.test(nb_test_episodes)
+                done = True  # testing function used env, we know need to reset
+                if verbose:
+                    print(
+                        "Timestep: {0:6d}/{1:6d}    Mean test return: {2:6.2f}    Epsilon: {3:3.2f}    Loss: {4:4.2f}".format(
+                            timestep, nb_timesteps, test_return, epsilon, loss
+                        )
                     )
-                    # loss = target - Q(s′,a′)  (stop grad of target)
-                    loss = self.criterion(
-                        target.detach(),
-                        torch.index_select(self.main_Q(states), 1, actions),
-                    )
-                    # Optimize the model : theta <- theta - grad theta
-                    self.optimizer.zero_grad()
-                    loss.backward()
-                    self.optimizer.step()
-                    # broadcast the network to update every workers
-                    MPI.COMM_WORLD.bcast(self.main_Q.state_dict())
-                else:  # on non-root workers
-                    # recieved and update network
-                    state_dict = MPI.COMM_WORLD.bcast(None)
-                    self.main_Q.load_state_dict(state_dict)
-            if epoch % self.tau == 0:
-                self.target_Q.load_state_dict(self.main_Q.state_dict())
-            epsilon *= self.epsilon_decay
+                # print(self.Q_main(torch.FloatTensor([0.1,-0.1,0.2,0.1]))[0].item())
+                test_returns.append(self.Q_main(torch.FloatTensor([0.1,-0.1,0.2,0.1]))[0].item())
 
-            if self._is_mpi_root:  # do not execute test on non-root worker
-                success_rate = self.test(nb_test_rollout)
-                print(epoch, success_rate, epsilon)
-                success_rates.append(success_rate)
+        return test_returns
 
-        return success_rates
-
-    def test(self, nb_tests):
+    def test(self, nb_test_episodes):
         """Test the policy nb_tests times and return the mean reward rate."""
-        sum_rewards = []
-        for _ in range(nb_tests):
+        returns = []
+        for _ in range(nb_test_episodes):
             state = self.env.reset()
             done = False
             rewards = []
@@ -153,15 +145,5 @@ class DQN:
                 rewards.append(reward)
                 state = next_state
                 if done:
-                    sum_rewards.append(np.sum(rewards))
-
-        return np.mean(sum_rewards)
-
-    def play_once(self):
-        """Play once with rendering"""
-        state = self.env.reset()
-        done = False
-        while not done:
-            action = self.act(state, epsilon=0)
-            state, _, done, _ = self.env.step(action)
-            self.env.render()
+                    returns += rewards_to_returns(rewards, self.gamma)
+        return np.mean(returns)
--- examples/DQN_CartPole-v1.py
+++ examples/DQN_CartPole-v1.py
@@ -2,10 +2,15 @@ import gym
 from deep_rl.algorithms import DQN
 
 
-env = gym.make("CartPole-v1")
-results = []
 for _ in range(20):
+    env = gym.make("CartPole-v1")
     agent = DQN(env)
-    results.append(agent.train(3000000, 10000))
+    print(agent.train(60000, 1000, verbose=False))
 
-print(results)
+# for _ in range(100):
+#     state = env.reset()
+#     done = False
+#     while not done:
+#         action = agent.act(state)
+#         state, _, done, _ = env.step(action)
+#         env.render()
