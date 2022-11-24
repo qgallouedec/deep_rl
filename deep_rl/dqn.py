05a776d4c03b34b9f07569512c7b338f3aff5784
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-11-24 17:57:35 +0100

    remove device

--- deep_rl/dqn.py
+++ deep_rl/dqn.py
@@ -11,18 +11,14 @@ class TorchWrapper(gym.Wrapper):
     Torch wrapper. Actions and observations are Tensors instead of arrays.
     """
 
-    def __init__(self, env: gym.Env, device: Optional[Union[torch.device, str]] = None) -> None:
-        super().__init__(env)
-        self.device = device
-
     def step(self, action: Tensor) -> Tuple[Tensor, float, bool, Dict[str, Any]]:
         action = action.cpu().numpy()
         observation, reward, done, info = self.env.step(action)
-        return torch.tensor(observation).to(self.device), reward, done, info
+        return torch.tensor(observation), reward, done, info
 
     def reset(self) -> Tensor:
         observation = self.env.reset()
-        return torch.tensor(observation).to(self.device)
+        return torch.tensor(observation)
 
 
 class QNetwork(nn.Module):
