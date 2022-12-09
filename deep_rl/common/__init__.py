c0837a3f99f51536afb89f320442a2dbdcb0b911
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-09 22:05:39 +0100

    rm unused

--- deep_rl/common/__init__.py
+++
@@ -1,2 +0,0 @@
-from .nn import MLP
-from .utils import rewards_to_returns
--- deep_rl/reinforce.py
+++ deep_rl/reinforce.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Tuple
 
 import gym
 import numpy as np
